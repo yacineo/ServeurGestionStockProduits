c558d244a2207b9e4dea1ac09d3dd62eb9c0cdf6
yacineo <oucheneyacine310@gmail.com>
2019-01-03 00:24:50 +0100

    crud produits

---
+++ src/main/java/com/example/demo/service/IProduitService.java
@@ -0,0 +1,17 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.entity.Produit;
+
+public interface IProduitService {
+
+	List<Produit> getProduits();
+	
+	
+	void addProduit(Produit produit);
+	void updateProduit(Produit produit);
+	void deleteProduit(String ref);
+		
+	
+}
---
+++ src/main/java/com/example/demo/service/ProduitMockServiceImpl.java
@@ -0,0 +1,49 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Produit;
+
+@Service
+public class ProduitMockServiceImpl implements IProduitService {
+
+	private List<Produit> produits;
+
+	public ProduitMockServiceImpl() {
+		produits = new ArrayList<Produit>();
+		produits.add(new Produit("Livre", 50, 20));
+		produits.add(new Produit("Cahier", 10, 30));
+		produits.add(new Produit("Stylo", 5, 50));
+	}
+
+	@Override
+	public List<Produit> getProduits() {
+
+		return produits;
+	}
+
+	@Override
+	public void addProduit(Produit produit) {
+		produits.add(produit);
+
+	}
+
+	@Override
+	public void updateProduit(Produit produit) {
+		produits.remove(produit);
+		produits.add(produit);
+
+	}
+
+	@Override
+	public void deleteProduit(String ref) {
+		Produit produit = new Produit();
+		produit.setRef(ref);
+		produits.remove(produit);
+
+	}
+
+}
